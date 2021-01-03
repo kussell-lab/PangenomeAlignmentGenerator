df5523b29faf8fb5c475c0468e3f65de808544bd
Asher Preska Steinberg <asherps@gmail.com>
2021-01-03 15:28:42 -0500

    APS158 commits; ConvertMapwSingularity.sh is identical to ConvertMap except it invokes Singularity to get smalt and sra-tools and includes the prefetch step here. Appears to be the most effective way to download and map; CollectPangenomeAlignments collects genomes that have been aligned to the pangenome FASTA file from Roary and makes a MSA file; other commits are submission scripts

---
+++ bin/ConvertMapwSingularity.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# This script maps reads to the reference genome to generate the consensus genomic sequence.
+# Created by Mingzhi Lin (mingzhi9@gmail.com) with edits by Asher Preska Steinberg (apsteinberg@nyu.edu)
+# It requires samtools (https://github.com/samtools/samtools) and smalt (http://www.sanger.ac.uk/science/tools/smalt-0).
+# Inputs:
+#   (1) the accession list file;
+#   (2) the working directory;
+#   (3) the reference genome.
+#   (4) number of threads given to mapping/compression from sam to bam
+# Output:
+#   the consensus genomic sequences in FASTA format.
+
+function map2reference {
+    READ=$1
+    WORKING_DIR=$2
+    REFERENCE=$3
+    LABEL=pileup
+    sra_file=${WORKING_DIR}/${READ}/${READ}.sra
+    singularity exec /home/aps376/sra-tools.sif prefetch ${READ}
+    singularity exec /home/aps376/sra-tools.sif fastq-dump -O ${WORKING_DIR} --split-3 ${sra_file}
+    singularity exec /home/aps376/smalt.sif smalt map -n 1 ${REFERENCE} ${WORKING_DIR}/${READ}_1.fastq ${WORKING_DIR}/${READ}_2.fastq | \
+        samtools sort -T /tmp -o ${WORKING_DIR}/${READ}.sorted.bam
+        samtools mpileup -f ${REFERENCE} ${WORKING_DIR}/${READ}.sorted.bam | \
+        GenomicConsensus --fna_file ${REFERENCE} > ${WORKING_DIR}/${READ}.${LABEL}.fasta
+        rm ${WORKING_DIR}/${READ}.sorted.bam
+        rm ${WORKING_DIR}/${READ}_1.fastq
+        rm ${WORKING_DIR}/${READ}_2.fastq
+        rm -r ${WORKING_DIR}/${READ}
+}
+
+export -f map2reference
+
+accession_list_file=$1
+working_dir=$2
+reference=$3
+singularity exec /home/aps376/smalt.sif smalt index ${reference} ${reference}
+samtools faidx ${reference}
+parallel map2reference {} ${working_dir} ${reference} :::: ${accession_list_file}
